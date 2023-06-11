d37185d180608ada35c3ed24722f6c33df70bdca
Tommie Olislagers <i457616@fhict.nl>
2023-06-11 15:59:48 +0200

    Apperently there was no :latest at the frontend image??

---
+++ docker_postgres_init_users.sql
@@ -0,0 +1,12 @@
+CREATE EXTENSION IF NOT EXISTS "uuid-ossp";
+CREATE TABLE users
+(
+    Id uuid PRIMARY KEY DEFAULT UUID_GENERATE_V4(),
+    Username text NOT NULL,
+    Password text NOT NULL,
+    Email text NOT NULL,
+    Role text
+);
+
+INSERT INTO users(Username, Password, Email, Role) VALUES
+ ('TommieOlislagers', 'Test12345!', '457616@student.fontys.nl', 'Administrator');
